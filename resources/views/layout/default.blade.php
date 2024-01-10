359a1a1b99af8b313e9dc99d2571f5a591c54ea3
ansonsarturs <65274330+arturs-ansons@users.noreply.github.com>
2024-01-10 19:57:10 +0200

    Update default.blade.php

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -16,7 +16,3 @@
 </body>
 </html>
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
